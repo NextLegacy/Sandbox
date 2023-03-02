52b7bf6e45d95846124dad361d148790f395129d
NextLegacy <mcserezli@gmail.com>
2023-03-02 23:57:46 +0100

    snake class for snake game

--- Java/src/snake/Snake.java
+++ Java/src/snake/Snake.java
@@ -1,8 +1,21 @@
 package snake;
 
 import engine.Script;
+import engine.graphics.Image;
 
 public class Snake extends Script
 {
-    
+    SnakeScene scene;
+
+    @Override
+    protected void start() 
+    {
+        scene = scene();
+    }
+
+    @Override
+    protected void render() 
+    {
+        image().mesh(scene.camera, scene.cube, transform().transformationMatrix(), Image.fromFile("./rsc/snake/snake.jpeg"));
+    }
 }
