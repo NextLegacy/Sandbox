9a8b9737e7063e657830e8941a14f8520bc83a25
NextLegacy <mcserezli@gmail.com>
2023-03-12 23:59:00 +0100

    LoggerInformations when Logging

---
+++ Java/src/logger/LoggerInformation.java
@@ -0,0 +1,45 @@
+package logger;
+
+import color.ANSICode;
+
+public final class LoggerInformation 
+{
+    private String level;
+
+    private final LoggerLabel[] labels;
+
+    public LoggerInformation(final LoggerLabel[] labels) 
+    { 
+        this.labels = labels;
+
+        level = "NONE";
+    }
+
+    public final LoggedString useOn(final String message, final String join) 
+    { 
+        String information = "";
+        
+        ANSICode messageANSICode = ANSICode.NONE;
+
+        for (int i = 0; i < labels.length; i++)
+        {
+            if (!labels[i].condition.get(level)) continue;
+
+            information += labels[i].getLabel(level) + join;
+
+            if (labels[i].messageColorCode != ANSICode.NONE)
+                messageANSICode = labels[i].messageColorCode;
+        }
+
+        return new LoggedString
+        (
+            level,
+            information + messageANSICode + message + ANSICode.NONE
+        );
+    }
+
+    public final String getLevel() { return level; }
+    public final LoggerInformation setLevel(String level) { this.level = level; return this; }
+
+    public static final LoggerInformation NONE = new LoggerInformation(new LoggerLabel[0]);
+}
---
+++ Java/src/logger/LoggerLabel.java
@@ -0,0 +1,22 @@
+package logger;
+
+import engine.utils.Lambda.Func1;
+import color.ANSICode;
+
+public class LoggerLabel 
+{
+    final Func1<Boolean, String> condition;
+    final ANSICode labelColorCode;
+    final ANSICode messageColorCode;
+    final Func1<String, String> label;
+
+    public LoggerLabel(Func1<Boolean, String> condition, Func1<String, String> label, ANSICode labelColorCode, ANSICode messageColorCode) 
+    { 
+        this.condition        = condition        == null ? (level) -> true : condition       ;
+        this.labelColorCode   = labelColorCode   == null ? ANSICode.NONE   : labelColorCode  ;
+        this.messageColorCode = messageColorCode == null ? ANSICode.NONE   : messageColorCode;
+        this.label            = label            == null ? (level) -> ""   : label           ;
+    }
+
+    final String getLabel(String level) { return labelColorCode + label.get(level) + ANSICode.NONE; }
+}
