2a98693f7aa54a06d45458b2b74afdec8479b9a3
NextLegacy <mcserezli@gmail.com>
2023-03-12 23:59:11 +0100

    TicTacToe game

--- Java/src/tictactoe/TicTacToe.java
+++ Java/src/tictactoe/TicTacToe.java
@@ -5,32 +5,123 @@ package tictactoe;
 // Not finished yet, and gonna rethink it, but for now discountinued
 public class TicTacToe 
 {
-    //field = 0bT__AAA_AAA_AAA_BBB_BBB_BBB | T = turn | A = used field | B = player
-    private int field = 0b0__000_000_000_000_000_000;
-    private int turn  = 0b1__000_000_000_000_000_000;
+    //field = 0bT__BBB_BBB_BBB_AAA_AAA_AAA | T = turn | A = used field | B = player
+    private int game = 0b0__000_000_000_100_100_100;
+
+    private final int winningGames[] = new int[] 
+    {
+        0b0_000_000_111_000_000_111,
+        0b0_000_111_000_000_111_000,
+        0b0_111_000_000_111_000_000,
+        
+        0b0_100_100_100_100_100_100,
+        0b0_010_010_010_010_010_010,
+        0b0_001_001_001_001_001_001,
+
+        0b0_100_010_001_100_010_001,
+        0b0_001_010_100_001_010_100
+    };
 
     public static void main(String[] args)
     {
-        TicTacToe t = new TicTacToe();
-
-        t.makeMove(1);
-        t.makeMove(2);
-        t.makeMove(2);
-        t.makeMove(2);
-        t.makeMove(1);
-        t.makeMove(1);
-
-        System.out.println(Integer.toBinaryString(t.field));
-    }
-
-    public int turn() {
-        return turn;
-    }
-
-    public void makeMove(int pos)
-    {
-        //field = field | (((field & 0b1) ? (0b1 << pos) : 0) | (0b1 << (pos + 9)));
+        TicTacToe ticTacToe = new TicTacToe();
         
-        turn = turn ^ 0b1;
+        System.out.println(ticTacToe.fieldAsString());
+
+
+        System.out.println(ticTacToe.fieldAsString());
+
+        System.out.println(ticTacToe.gameAsBinaryString());
+        
+        System.out.println(ticTacToe.checkWinner());
+    }
+
+    private int field()
+    {
+        return (game & 0b0__111_111_111_111_111_111);
+    }
+
+    private int player()
+    {
+        return (game & 0b0__111_111_111_000_000_000) >> 9;
+    }
+
+    private int player(int index)
+    {
+        index += 9;
+
+        return (game & (0b0__000_000_000_000_000_001 << index)) >> index;
+    }
+
+    private int used()
+    {
+        return (game & 0b0__000_000_000_111_111_111);
+    }
+
+    private int used(int index)
+    {
+        return (game & (0b0__000_000_000_000_000_001 << index)) >> index;
+    }
+
+    private int turn()
+    {
+        return game >>> 18;
+    }
+
+    private void nextTurn()
+    {
+        game ^= 0b1__000_000_000_000_000_000;
+    }
+
+    public void makeMove(int index)
+    {
+        if (used(index) == 0)
+        {
+            game |= 0b0__000_000_000_000_000_001 << index;
+            game |= turn() << (index + 9);
+        }
+
+        nextTurn();
+    }
+
+    public String fieldAsString()
+    {
+        String result = 
+            ((used(0) == 0) ? "[ ]" : (player(0) == 0) ? "[X]" : "[O]") +
+            ((used(1) == 0) ? "[ ]" : (player(1) == 0) ? "[X]" : "[O]") +
+            ((used(2) == 0) ? "[ ]" : (player(2) == 0) ? "[X]" : "[O]") + "\n" +
+            ((used(3) == 0) ? "[ ]" : (player(3) == 0) ? "[X]" : "[O]") +
+            ((used(4) == 0) ? "[ ]" : (player(4) == 0) ? "[X]" : "[O]") +
+            ((used(5) == 0) ? "[ ]" : (player(5) == 0) ? "[X]" : "[O]") + "\n" +
+            ((used(6) == 0) ? "[ ]" : (player(6) == 0) ? "[X]" : "[O]") +
+            ((used(7) == 0) ? "[ ]" : (player(7) == 0) ? "[X]" : "[O]") +
+            ((used(8) == 0) ? "[ ]" : (player(8) == 0) ? "[X]" : "[O]") + "\n";
+        
+        return result;
+    }
+
+    public String gameAsBinaryString()
+    {
+        return Integer.toBinaryString(game);
+    }
+
+    public int checkWinner()
+    {
+        int player1 = game & 0b0__111_111_111_111_111_111;
+        int player2 = ((~(game & 0b0__111_111_111_000_000_000)) & 0b0__111_111_111_111_111_111) | (game & 0b0__000_000_000_111_111_111);
+
+        System.out.println("player1: " + Integer.toBinaryString(player1));
+        System.out.println("player2: " + Integer.toBinaryString(player2));
+
+        for (int i = 0; i < winningGames.length; i++)
+        {
+            if (( player1 &  winningGames[i]) == winningGames[i] ||
+                (~player2 & ~winningGames[i]) == winningGames[i])
+            {
+                return turn();
+            }
+        }
+
+        return -1;
     }
 }
